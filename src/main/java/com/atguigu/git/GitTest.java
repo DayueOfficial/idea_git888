3667462f9accbee8b0a838b9123bbd6d071fd2ad
大跃 <hanzong188@gmail.com>
2022-09-26 09:33:27 +0800

    master分支在第9行添加

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("第一次更新");
         System.out.println("第二次更新");
         System.out.println("dev分支添加");
+        System.out.println("master分支在第9行添加");
     }
 }
